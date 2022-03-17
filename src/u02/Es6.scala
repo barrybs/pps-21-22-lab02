aba8a11d89ff444c5c58c1c6b8d4ed48bc4668fc
Barry Bassi <barry.bassi@studio.unibo.it>
2022-03-17 13:11:28 +0100

    Comment errata

--- src/u02/Es6.scala
+++ src/u02/Es6.scala
@@ -3,8 +3,8 @@ import Math._
 
 object Es6
   /*Ex6
-  Non è una ricorsione tail perché non fibo non viene richiamata solo alla fine. Inoltre utilizzando la libreria tailrec
-  viene segnalata come non ricorsiva
+  Non è una ricorsione tail perché  fibo non viene richiamata solo alla fine. Inoltre utilizzando la libreria tailrec
+  viene segnalata come non ricorsiva tail
   */
   def fibo (n:Int): Int = n match
     case n if n<= 1 => n
